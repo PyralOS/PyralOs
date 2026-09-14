a40573f4178dcbbd8dba54b9a6fc8a5ae174661f
KURWAss <kurwa.offc@proton.me>
2026-09-14 20:49:12 +0300

    Added multiply and divide operation into calculator

--- apps/calc.c
+++ apps/calc.c
@@ -7,3 +7,15 @@ int add(int a, int b) {
 int minus(int a, int b) {
     return a - b;
 }
+
+int multiply(int a, int b) {
+    return a * b;
+}
+
+int divide(int a, int b) {
+    if (a != 0 && b != 0) {
+        return a / b;
+    } else {
+        return;
+    }
+}
--- apps/calc.h
+++ apps/calc.h
@@ -3,5 +3,7 @@
 
 int add(int a, int b);
 int minus(int a, int b);
+int multiply(int a, int b);
+int divide(int a, int b);
 
 #endif
